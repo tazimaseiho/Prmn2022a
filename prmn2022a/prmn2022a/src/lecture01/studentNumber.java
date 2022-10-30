5ccaaecc364d3133d11c9b03df14577e2b3830f3
[tazimaseiho] <[b2211540@photon.chitose.ac.jp]>
2022-10-30 21:09:54 +0900

    課題1-2

---
+++ prmn2022a/prmn2022a/src/lecture01/studentNumber.java
@@ -0,0 +1,9 @@
+package lecture01;
+
+public class studentNumber {
+    public static void main(String[] args) {
+        int studentNumber = 2211540;
+        System.out.println("B"+studentNumber+" Tazima Seiho");
+
+    }
+}
