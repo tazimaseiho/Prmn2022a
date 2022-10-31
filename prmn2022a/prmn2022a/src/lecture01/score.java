f7718ec70e56e457f7e294ded331b642d1e13728
[tazimaseiho] <[b2211540@photon.chitose.ac.jp]>
2022-10-31 20:50:12 +0900

    課題1-5

--- prmn2022a/prmn2022a/src/lecture01/score.java
+++ prmn2022a/prmn2022a/src/lecture01/score.java
@@ -3,15 +3,12 @@ package lecture01;
 
 public class score {
     public static void main(String[] args) {
-        int[] score = {41,85,72,38,80};
-        int max = score[0];
-        int min = score[0];
-        int sum = 0;
+        int[] score = {41, 85, 72, 38, 80};
 
         for (int i = 0; i < score.length; i++) {
             if (score[i] >= 90) {
                 System.out.println(i + "番 " + score[i] + "点 " + "秀");
-            }else if (score[i] >= 80) {
+            } else if (score[i] >= 80) {
                 System.out.println(i + "番 " + score[i] + "点 " + "優");
             } else if (score[i] >= 70) {
                 System.out.println(i + "番 " + score[i] + "点 " + "良");
@@ -21,21 +18,35 @@ public class score {
                 System.out.println(i + "番 " + score[i] + "点 " + "不可");
             }
         }
-        for (int i : score)
+        max(score);
+        min(score);
+        average(score);
+    }
+
+    static void max(int[] a) {
+        int max = a[0];
+        for (int i : a) {
             if (i > max) {
                 max = i;
             }
-        for (int i : score) {
-            if (i < min) {
-                min = i;
-            }
         }
-        for (int i : score) {
-            sum = sum + i;
-        }
-        float average = (float)sum/score.length;
         System.out.println("最高点:" + max + "点");
-        System.out.println("最低点:" + min + "点");
-        System.out.println("平均点:" + average + "点");
     }
-}
+        static void min (int[] a){
+            int min = a[0];
+            for (int i : a) {
+                if (i < min) {
+                    min = i;
+                }
+            }
+            System.out.println("最低点:" + min + "点");
+        }
+        static void average (int[] a){
+            int sum = 0;
+            for (int i : a) {
+                sum = sum + i;
+            }
+            double average = (double) sum / a.length;
+            System.out.println("平均点:" + average + "点");
+        }
+    }
