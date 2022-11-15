2c9c9fef57c99b41def2a8d2117f716fbea9b474
[tazimaseiho] <[b2211540@photon.chitose.ac.jp]>
2022-11-15 22:29:34 +0900

    課題3-1

--- prmn2022a/prmn2022a/src/lecture03/Exercise3_1.java
+++ prmn2022a/prmn2022a/src/lecture03/Exercise3_1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Exercise3_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.print("任意の文字列を入力してください:" );
         String i = input.next();
         System.out.println(i + "と入力されました");
