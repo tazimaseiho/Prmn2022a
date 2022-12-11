511f9295c15e8bc249d46f22403e0cdef66814a8
[tazimaseiho] <[b2211540@photon.chitose.ac.jp]>
2022-12-11 22:41:36 +0900

    課題6-1

--- prmn2022a/prmn2022a/src/lecture06/Exercise6_1.java
+++ prmn2022a/prmn2022a/src/lecture06/Exercise6_1.java
@@ -8,7 +8,7 @@ public class Exercise6_1 {
         Scanner input = new Scanner(System.in);
         try {
             System.out.print("小数値を入力してください:");
-             Double a = input.nextDouble();
+             double a = input.nextDouble();
             System.out.println("入力した値:" + a);
         }catch (InputMismatchException e){
             System.out.println("エラー.");
